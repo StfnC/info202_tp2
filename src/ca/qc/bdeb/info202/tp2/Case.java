54442c38061fbaaed6372a8ce2f4219c825050b2
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-04-11 21:15:06 -0400

    Defined Case class

--- src/ca/qc/bdeb/info202/tp2/Case.java
+++ src/ca/qc/bdeb/info202/tp2/Case.java
@@ -1,4 +1,15 @@
 package ca.qc.bdeb.info202.tp2;
 
 public abstract class Case {
+    private String nom;
+    private String description;
+
+    public Case(String nom, String description) {
+        this.nom = nom;
+        this.description = description;
+    }
+
+    public abstract void effectuerAction(Joueur joueur);
+
+    public abstract void survolerCase(Joueur joueur);
 }
--- src/ca/qc/bdeb/info202/tp2/PlateauJeu.java
+++ src/ca/qc/bdeb/info202/tp2/PlateauJeu.java
@@ -3,4 +3,6 @@ package ca.qc.bdeb.info202.tp2;
 import java.io.Serializable;
 
 public class PlateauJeu implements Serializable {
+    private final int NB_CASES_PLATEAU = 10;
+
 }
