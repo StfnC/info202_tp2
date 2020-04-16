17359e6e327252d2c83fa0d6ff03a5ac07d06a58
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-04-16 12:19:39 -0400

    Implemented StationnementGratuit class

--- src/ca/qc/bdeb/info202/tp2/StationnementGratuit.java
+++ src/ca/qc/bdeb/info202/tp2/StationnementGratuit.java
@@ -1,4 +1,17 @@
 package ca.qc.bdeb.info202.tp2;
 
 public class StationnementGratuit extends Case {
+    public StationnementGratuit(String nom, String description) {
+        super(nom, description);
+    }
+
+    @Override
+    public void effectuerAction(Joueur joueur) {
+
+    }
+
+    @Override
+    public void survolerCase(Joueur joueur) {
+
+    }
 }
