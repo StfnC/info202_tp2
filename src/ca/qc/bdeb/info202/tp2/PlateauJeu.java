84259917c5dca1f8bbed165b685dafdf0bceb835
Stefan Cotargasanu <cotargasanu.stef@gmail.com>
2020-04-16 12:31:05 -0400

    Implemented PlateauJeu class

--- src/ca/qc/bdeb/info202/tp2/PlateauJeu.java
+++ src/ca/qc/bdeb/info202/tp2/PlateauJeu.java
@@ -3,6 +3,19 @@ package ca.qc.bdeb.info202.tp2;
 import java.io.Serializable;
 
 public class PlateauJeu implements Serializable {
-    private final int NB_CASES_PLATEAU = 10;
+    private static final int NB_CASES_PLATEAU = 10;
+    private Case[] casesPlateau;
 
+    public PlateauJeu(Case[] casesPlateau) {
+        // TODO: -Maybe make sure the board is the right size
+        this.casesPlateau = casesPlateau;
+    }
+
+    public static int getNbCasesPlateau() {
+        return NB_CASES_PLATEAU;
+    }
+
+    public Case[] getCasesPlateau() {
+        return this.casesPlateau;
+    }
 }
